bdb66767ef8130b9c7c921842ef6e51521178948
madhuv2002 <madhuv2002@gmail.com>
2021-12-03 09:00:04 -0600

    Create getters and setters for edge class

--- edge.cpp
+++ edge.cpp
@@ -4,8 +4,32 @@
 using namespace std;
 
 Edge::Edge(StopPoint begin_, StopPoint final_)
-        : start(begin_), end(final_), name(start.getStopID() + end.getStopID())
-    { }
+    : start_(begin_), end_(final_), name_(start_.getStopID() + end_.getStopID()), weight_(1)
+{ }
 
-Edge::Edge() : start(), end(), name("")
-    { }
+Edge::Edge() : start_(), end_(), name_(""), weight_(0)
+{ }
+
+void Edge::setStartPoint(StopPoint t) {
+    start_ = t;
+}
+
+StopPoint Edge::getStartPoint() {
+    return start_;
+}
+
+void Edge::setEndPoint(StopPoint t) {
+    end_ = t;
+}
+
+StopPoint Edge::getEndPoint() {
+    return end_;
+}
+
+void Edge::setWeight(double t) {
+    weight_ = t;
+}
+
+double Edge::getWeight() {
+    return weight_;
+}
