dd8ef04ea3b113e5a36715739c6aa536627f2062
Arnav Shah <arnavss2@siebl-0224-20.ews.illinois.edu>
2021-12-05 15:17:12 -0600

    Implement loadEdges function

--- routemap.cpp
+++ routemap.cpp
@@ -48,7 +48,7 @@ void RouteMap::loadNode(string fileName) {
                      //std::cout << slicedStop.at(count) << '\n';
                     StopPoint stp(slicedStop.at(count), slicedStop.at(count + 1), slicedStop.at(count + 2),
                      slicedStop.at(count + 3), slicedStop.at(count + 4));
-                     vertexMap.insert({slicedStop.at(count), stp});
+                     vertexMap.insert({slicedStop.at(count + 1), stp});
                      
                     count += 5;
                 } else {
@@ -66,5 +66,53 @@ void RouteMap::loadEdges() {
 }
 
 void RouteMap::loadEdges(std::string fileName) {
+    std::ifstream file;
 
+    file.open(fileName);
+    if (file.is_open()) {
+        for (std::string line; std::getline(file, line); ) {
+            // uncomment to view every stop
+            //std::cout << line << '\n';
+
+            std::vector<std::string> slicedTime1 =  tokenize(line, ",");
+            std::getline(file, line);
+            std::vector<std::string> slicedStop1 = tokenize(line, ",");
+            std::getline(file, line);
+            std::vector<std::string> slicedTime2 =  tokenize(line, ",");
+            std::getline(file, line);
+            std::vector<std::string> slicedStop2 = tokenize(line, ",");
+
+            std::string name_ = slicedStop1[1] + slicedStop2[1]
+            StopPoint stp1(slicedStop1[0], slicedStop1[1], slicedStop1[2], slicedStop1[3]);
+            StopPoint stp2(slicedStop2[0], slicedStop2[1], slicedStop2[2], slicedStop2[3]);
+
+            int count = 0;
+
+            // uncomment to view every stop point
+            // int idx = 0;
+            // for (std::string elem : slicedStop) {
+            //     if (elem.empty()) {
+            //         continue;
+            //         //std::cout << slicedStop.erase(slicedStop.begin() + (idx)) << '\n';
+            //     } else {
+            //         std::cout << elem << '\n';
+            //     }
+            //     idx++;
+            // }
+
+            while (count < slicedStop.size()) {
+                if (count > 3 && (count + 5) < slicedStop.size() && !slicedStop.at(count).empty()) {
+                     //std::cout << slicedStop.at(count) << '\n';
+                    StopPoint stp(slicedStop.at(count), slicedStop.at(count + 1), slicedStop.at(count + 2),
+                     slicedStop.at(count + 3), slicedStop.at(count + 4));
+                     vertexMap.insert({slicedStop.at(count + 1), stp});
+                     
+                    count += 5;
+                } else {
+                    count++;
+                }
+            }
+
+        }
+    }
 }
