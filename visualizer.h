13f6ad4273b1b1f05ff1bef16948478afa44f2ba
madhuv2002 <madhuv2002@gmail.com>
2021-12-06 16:21:10 -0600

    Create visualizer class

---
+++ visualizer.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include "stoppoint.h"
+#include "edge.h"
+#include "routemap.h"
+#include "cs225/PNG.h"
+using namespace cs225;
+
+class Visualizer {
+    public:
+    // distance, set location for all the points, draw, map of node to points, 
+    // function to calculate weight of node 
+
+
+    private:
+    std::map<StopPoint p1, StopPoint p2>;
+
+};
