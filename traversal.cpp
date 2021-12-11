acafdf9cbd1d00b4dbf418f5548bfabf0c766cec
Arnav Shah <arnavss2@siebl-0224-43.ews.illinois.edu>
2021-12-11 11:04:23 -0600

    Implemented BFS travesal

--- traversal.cpp
+++ traversal.cpp
@@ -1,1 +1,33 @@
 #include "traversal.h"
+#include <queue>
+#include <string>
+
+vector<string> Traversal::BFS(string s) {
+    map<string, bool> visited;
+    for (auto pair: route.getVertexMap()) {
+        visited[pair.first] = false;
+    }
+
+    queue<string> q;
+
+    visited[s] = true;
+    q.push(s);
+
+    vector<string> r;
+
+    while (!q.empty()) {
+        s = q.front();
+        r.push_back(s);
+        q.pop();
+
+        for (Edge edge: route.getVertexMap()[s].second) {
+            string i = edge.getStartPoint().getStopName();
+            if (!visited[i]) {
+                visited[i] = true;
+                q.push(i);
+            }
+        }
+    }
+
+    return r;
+}
--- traversal.h
+++ traversal.h
@@ -4,5 +4,18 @@
 #include "cs225/PNG.h"
 #include <fstream>
 #include <string>
+#include <vector>
+#include <map>
 #include "visualizer.h"
+#include "edge.h"
 using namespace std;
+
+class Traversal {
+    public:
+
+    private:
+        RouteMap route;
+
+        vector<string> BFS(string s);
+        
+}
