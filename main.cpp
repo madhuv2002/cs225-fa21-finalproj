482f15ad8cccc71f3a5a795f9035e12075557116
Arnav Shah <arnavss2@siebl-0224-11.ews.illinois.edu>
2021-12-13 21:49:17 -0600

    Modified main file for inputs

--- main.cpp
+++ main.cpp
@@ -3,15 +3,19 @@
 #include "cs225/PNG.h"
 #include <fstream>
 #include <string>
-
+#include "traversal.h"
 #include "visualizer.h"
+#include <iostream>
+#include <fstream>
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
+    //making the graph
     RouteMap route;
     route.loadNode("./assets/stops.csv");
     route.loadEdges();
     
+    //drawing the visualization
     Visualizer v;
     v.findLocation(route);
 
@@ -19,5 +23,22 @@ int main() {
     png->writeToFile("map.png");
 
     delete png;
+
+    //traversing the graph
+    Traversal t(route);
+
+    vector<string> bfs = t.BFS(argv[1]);
+    ofstream myfile;
+    myfile.open ("bfs.txt");
+    for (string v: bfs) {
+        myfile <<  v << "\n";
+    }
+    myfile.close();
+    
+    vector<Edge> edges = t.findShortestPath(argv[1], argv[2]);
+    
+    for (Edge edge: edges) {
+        cout << edge.getName() << endl;
+    }
     return 0;
 }
